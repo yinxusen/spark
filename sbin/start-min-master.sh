8805bad5cec8e0054b9243644b2611c3c3bc308a
Xusen Yin <yinxusen@gmail.com>
2014-06-14 15:25:55 +0800

    add some variables

--- sbin/start-min-master.sh
+++ sbin/start-min-master.sh
@@ -2,15 +2,15 @@
 
 set -e -x
 
-IP="127.0.0.1"
-PORT="7077"
-WEB_PORT="8080"
+sbin=`dirname $0`
+sbin=`cd $sbin; pwd`
 
-DEPS_JARS="../assembly/target/scala-2.10/spark-assembly-1.1.0-SNAPSHOT-hadoop1.0.4-deps.jar"
-CORE_CLASSES="../core/target/scala-2.10/classes"
-EXAMPLES_CLASSES="../examples/target/scala-2.10/classes"
-REPL_CLASSES="../repl/target/scala-2.10/classes"
-TOOLS_CLASSES="../tools/target/scala-2.10/classes"
+SPARK_MASTER_IP=`hostname`
+SPARK_MASTER_PORT=7077
+WEBUI_PORT=8080
+
+DEPS_JARS="$sbin/../assembly/target/scala-2.10/spark-assembly-1.1.0-SNAPSHOT-hadoop1.0.4-deps.jar"
+CORE_CLASSES="$sbin/../core/target/scala-2.10/classes"
 
 CP="$DEPS_JARS:$CORE_CLASSES"
 
--- sbin/start-min-worker.sh
+++ sbin/start-min-worker.sh
@@ -2,18 +2,15 @@
 
 set -e -x
 
-IP="127.0.0.1"
-PORT="7077"
-WEB_PORT="8080"
+SPARK_MASTER_IP=`hostname`
+SPARK_MASTER_PORT=7077
+WEBUI_PORT=8080
 
 DEPS_JARS="../assembly/target/scala-2.10/spark-assembly-1.1.0-SNAPSHOT-hadoop1.0.4-deps.jar"
 CORE_CLASSES="../core/target/scala-2.10/classes"
-EXAMPLES_CLASSES="../examples/target/scala-2.10/classes"
-REPL_CLASSES="../repl/target/scala-2.10/classes"
-TOOLS_CLASSES="../tools/target/scala-2.10/classes"
 
 CP="$DEPS_JARS:$CORE_CLASSES"
 
 export SPARK_HOME=/home/sen/git-store/spark
 
-java -cp $CP org.apache.spark.deploy.worker.Worker spark://127.0.0.1:7077
+java -cp $CP org.apache.spark.deploy.worker.Worker spark://$IP:$PORT
