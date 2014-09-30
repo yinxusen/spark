b18e9c4a1613c26b9a72bd24a605fd2f9e7a436c
lanwei <panda_run@qq.com>
2014-09-30 20:37:00 +0800

    add comments

--- graphx/src/main/scala/org/apache/spark/graphx/lib/community/PregelUnfolding.scala
+++ graphx/src/main/scala/org/apache/spark/graphx/lib/community/PregelUnfolding.scala
@@ -16,10 +16,8 @@ object PregelUnfolding {
 
   def run[VD: ClassTag, ED: ClassTag](graph: Graph[VD, ED], maxIter: Int): Graph[Set[VertexId], ED] = {
 
-    //each node belongs to its own community
-    var ufGraph = graph
     // m is the sum of all links in the graph
-    val m = graph.degrees.map(_._2).fold(0)(_ + _)
+    val m = graph.degrees.map(_._2).fold(0)(_ + _)/2
 
     var ufWorkGraph = graph.mapVertices((vid, _) => Set(vid))
 
@@ -27,76 +25,43 @@ object PregelUnfolding {
 
     while (iter < maxIter) {
       iter += 1
+      //sum of links inside the community composed of vertex in com
+      def countInside(com: Set[VertexId]): Long = ufWorkGraph.subgraph(vpred = (id, attr) =>
+        com.subsetOf(attr)).degrees.map(_._2).fold(0)(_ + _)/2
 
-      //Recording which community each vertex belongs to in this iteration
-      //ufGraph = ufGraph.joinVertices(ufWorkGraph.vertices)((vid, attr, cid) => cid :: attr :: Nil
+      //sum of links inside the community composed of vertex not in com
+      def countOutside(com: Set[VertexId]): Long = ufWorkGraph.subgraph(vpred = (id, attr) =>
+        !com.subsetOf(attr)).degrees.map(_._2).fold(0)(_ + _)/2
 
-      /*ufWorkGraph = Pregel(ufWorkGraph, initialMessage, activeDirection = EdgeDirection.Either)(
-        (vid, attr, message) => message.maxBy(x => modGained(vid, x)),
+      //sum of links inside the community composed of vertex both in com1 & com2
+      def countSumInside(com1: Set[VertexId], com2: Set[VertexId]): Long = ufWorkGraph
+        .subgraph(vpred = (id, attr) =>
+        com1.subsetOf(attr) || com2.subsetOf(attr)).degrees.map(_._2).fold(0)(_ + _)/2
 
-        e =>
-          if (gainMod(e.srcId, e.dstId) && e.srcId < e.dstId) {
-            Iterator((e.dstId, e.srcAttr))
-          } else if (gainMod(e.srcId, e.dstId) && e.dstId < e.srcId) {
-            Iterator(e.srcId, e.dstAttr)
-          } else {
-            Iterator()
-
-          },
-        (neighbor1, neighbor2) => (List(neighbor1) ++ List(neighbor2)).flatten
-      )*/
-      /*
-
-      def sendMessage(e: EdgeTriplet[VertexId, ED]) = {
-        if(gainMod(e.dstAttr, e.srcAttr)) {
-          Iterator((e.dstId, Set(e.srcAttr).union(Set(e.dstAttr))),
-            (e.srcId, Set(e.srcAttr).union(Set(e.dstAttr))))
-        }
-        else {
-          Iterator()
-        }
-      }
-
-      def mergeMessage(neighbor1: Set[VertexId], neighbor2: Set[VertexId]): List[Set[VertexId]] =
-        List(neighbor1) ++ List(neighbor2)
-
-      def vertexProgram(vid: VertexId, attr: Set[VertexId], message: List[Set[VertexId]]) =
-        message.maxBy(x => modGained(attr, x))
-        */
-
-      def countInside(com: Set[VertexId]): Long = ufWorkGraph.subgraph(vpred = (id, attr) => com.subsetOf(attr)).degrees
-        .map(_._2).fold(0)(_ + _)
-
-      def countOutside(com: Set[VertexId]): Long = ufWorkGraph.subgraph(vpred = (id, attr) => !com.subsetOf(attr)).degrees
-        .map(_._2).fold(0)(_ + _)
-
-      def countSumInside(com1: Set[VertexId], com2: Set[VertexId]): Long = ufWorkGraph.subgraph(vpred = (id, attr)
-      => com1.subsetOf(attr) || com2.subsetOf(attr)).degrees.map(_._2).fold(0)(_ + _)
-
-      //the modularity gained by moving neigh1 into neigh2
+      //compute the modularity gained by moving neigh1 into neigh2
       def modGained(neigh1: Set[VertexId], neigh2: Set[VertexId]): Long = {
-        // the sum of links inside neigh1-related community
+        //sum of links inside community composed of vertex in neigh1
         val inside1 = countInside(neigh1)
-        //the sum of links inside rest of the vertex except neigh1-related community
+        //um of links inside community composed of vertex not in neigh1
         val outside1 = countOutside(neigh1)
-
+        //sum of links inside community composed of vertex in neigh2
         val inside2 = countInside(neigh2)
-
+        //sum of links inside community composed of vertex not in neigh2
         val outside2 = countOutside(neigh2)
-
+        //sum of links connect two communities composed of vertex in neigh1 & neigh2 respectively
         val connect12 = countSumInside(neigh1, neigh2) - inside1 - inside2
-
+        //sum of links connect community composed of vertex in neigh2 & outside neigh2 respectively
         val connect2out = m - inside2 - outside2
-
+        //sum of links connect community composed of vertex in neigh1 & outside neigh1 respectively
         val connect1out = m - inside1 - outside1
-
+        //return the modularity gained by moving neigh1 into neigh2
         connect12 / (2 * m) - (connect2out + connect1out) /(2 * m * m)
 
       }
 
-      def gainMod(a: Set[VertexId], b: Set[VertexId]): Boolean = if (modGained(a, b) > 0) true else false
-
-
+      //whether putting community composed of com1 into com2 raise the modularity
+      def gainMod(com1: Set[VertexId], com2: Set[VertexId]): Boolean =
+        if (modGained(com1, com2) > 0) true else false
 
       val initialMessage = List[Set[VertexId]]()
       ufWorkGraph = Pregel(ufWorkGraph, initialMessage, activeDirection = EdgeDirection.Either)(
