95f5524597088008118e104c4fa5bbad0ce1376f
Xusen Yin <yinxusen@gmail.com>
2014-10-10 00:58:24 +0800

    use graph instead of ufWorkGraph

--- graphx/src/main/scala/org/apache/spark/graphx/lib/community/PregelUnfolding.scala
+++ graphx/src/main/scala/org/apache/spark/graphx/lib/community/PregelUnfolding.scala
@@ -23,94 +23,83 @@ import scala.reflect.ClassTag
 object PregelUnfolding {
 
   /**
-   * Count the number of edges of a sub-graph, given a vertex predication.
-   */
-  def numberOfEdges[ED: ClassTag](
-      ufWorkGraph: Graph[Set[VertexId], ED],
-      predicate: (VertexId, Set[VertexId]) => Boolean): Double = {
-    println(ufWorkGraph.vertices.collect())
-    val subgraph = ufWorkGraph.subgraph(vpred = predicate)
-    if (subgraph.vertices.count() == 0) {
-      println("error here")
-    }
-    subgraph.degrees.map(_._2).fold(0)(_ + _) / 2.0
-  }
-
-  /**
-   * Count the number of edges inside a community.
-   */
-  def countInnerEdges[ED: ClassTag](
-      ufWorkGraph: Graph[Set[VertexId], ED],
-      com: Set[VertexId]): Double = {
-    numberOfEdges(ufWorkGraph, (id, attr) => com.subsetOf(attr))
-  }
-
-  /**
-   * Count the number of edges of a sub-graph, which composed of all nodes not in the given
-   * community.
-   */
-  def countOuterEdges[ED: ClassTag](
-      ufWorkGraph: Graph[Set[VertexId], ED],
-      com: Set[VertexId]): Double = {
-    numberOfEdges(ufWorkGraph, (id, attr) => !com.subsetOf(attr))
-  }
-
-  /**
-   * Count the number of edges of a sub-graph, which composed of all nodes in the given two
-   * communities.
-   */
-  def countBipletInnerEdges[ED: ClassTag](
-      ufWorkGraph: Graph[Set[VertexId], ED],
-      com1: Set[VertexId],
-      com2: Set[VertexId]): Double = {
-    numberOfEdges(ufWorkGraph, (id, attr) => com1.subsetOf(attr) || com2.subsetOf(attr))
-  }
-
-  /**
-   * Compute the modularity-gain of merging lhs community with rhs one.
-   */
-  def modularityGain[ED: ClassTag](
-      ufWorkGraph: Graph[Set[VertexId], ED],
-      lhsComm: Set[VertexId],
-      rhsComm: Set[VertexId],
-      numGraphEdges: Long): Double = {
-    val lhsInnerEdges = countInnerEdges(ufWorkGraph, lhsComm)
-    val lhsOuterEdges = countOuterEdges(ufWorkGraph, lhsComm)
-    val rhsInnerEdges = countInnerEdges(ufWorkGraph, rhsComm)
-    val rhsOuterEdges = countOuterEdges(ufWorkGraph, rhsComm)
-
-    val bridgeEdges =
-      countBipletInnerEdges(ufWorkGraph, lhsComm, rhsComm) - lhsInnerEdges - rhsInnerEdges
-
-    val edgesConnectedToLHS = numGraphEdges - lhsInnerEdges - lhsOuterEdges
-    val edgesConnectedToRHS = numGraphEdges - rhsInnerEdges - rhsOuterEdges
-
-    bridgeEdges / (2 * numGraphEdges)
-      - (edgesConnectedToRHS * edgesConnectedToLHS) / (2 * numGraphEdges * numGraphEdges)
-  }
-
-  /**
-   * Check whether the modularity actually raise after merging two communities.
-   */
-  def isModularityGained[ED: ClassTag](
-      ufWorkGraph: Graph[Set[VertexId], ED],
-      lhsComm: Set[VertexId],
-      rhsComm: Set[VertexId],
-      numGraphEdges: Long): Boolean = {
-    if (modularityGain(ufWorkGraph, lhsComm, rhsComm, numGraphEdges) > 0) true else false
-  }
-
-  /**
    * TBD.
    */
   def run[VD: ClassTag, ED: ClassTag](
       graph: Graph[VD, ED],
       maxIter: Int): Graph[Set[VertexId], ED] = {
+
+    graph.cache()
     val numGraphEdges = graph.degrees.map(_._2).fold(0)(_ + _) / 2
 
-    var ufWorkGraph = graph.mapVertices((vid, _) => Set(vid))
+    /**
+    * Count the number of edges of a sub-graph, given a vertex predication.
+    */
+    def numberOfEdges(
+        predicate: (VertexId, VD) => Boolean): Double = {
+      val subgraph = graph.subgraph(vpred = predicate)
+      val vert = subgraph.vertices.collect()
+      subgraph.degrees.map(_._2).fold(0)(_ + _) / 2.0
+    }
 
-    println(ufWorkGraph.vertices.collect().mkString(","))
+    /**
+     * Count the number of edges inside a community.
+     */
+    def countInnerEdges(
+        com: Set[VertexId]): Double = {
+      numberOfEdges((id, _: VD) => com.contains(id))
+    }
+
+    /**
+     * Count the number of edges of a sub-graph, which composed of all nodes not in the given
+     * community.
+     */
+    def countOuterEdges(
+        com: Set[VertexId]): Double = {
+      numberOfEdges((id, _: VD) => ! com.contains(id))
+    }
+
+    /**
+     * Count the number of edges of a sub-graph, which composed of all nodes in the given two
+     * communities.
+     */
+    def countBipletInnerEdges(
+        com1: Set[VertexId],
+        com2: Set[VertexId]): Double = {
+      numberOfEdges((id, _: VD) => com1.contains(id) || com2.contains(id))
+    }
+
+    /**
+     * Compute the modularity-gain of merging lhs community with rhs one.
+     */
+    def modularityGain(
+        lhsComm: Set[VertexId],
+        rhsComm: Set[VertexId]): Double = {
+      val lhsInnerEdges = countInnerEdges(lhsComm)
+      val lhsOuterEdges = countOuterEdges(lhsComm)
+      val rhsInnerEdges = countInnerEdges(rhsComm)
+      val rhsOuterEdges = countOuterEdges(rhsComm)
+
+      val bridgeEdges =
+        countBipletInnerEdges(lhsComm, rhsComm) - lhsInnerEdges - rhsInnerEdges
+
+      val edgesConnectedToLHS = numGraphEdges - lhsInnerEdges - lhsOuterEdges
+      val edgesConnectedToRHS = numGraphEdges - rhsInnerEdges - rhsOuterEdges
+
+      bridgeEdges / (2 * numGraphEdges)
+        - (edgesConnectedToRHS * edgesConnectedToLHS) / (2 * numGraphEdges * numGraphEdges)
+    }
+
+    /**
+     * Check whether the modularity actually raise after merging two communities.
+     */
+    def isModularityGained(
+        lhsComm: Set[VertexId],
+        rhsComm: Set[VertexId]): Boolean = {
+      if (modularityGain(lhsComm, rhsComm) > 0) true else false
+    }
+
+    var ufWorkGraph = graph.mapVertices((vid, _) => Set(vid))
 
     var iter = 0
 
@@ -123,11 +112,11 @@ object PregelUnfolding {
           if (message.isEmpty) {
             attr
           } else {
-            message.maxBy(x => modularityGain(ufWorkGraph, attr, x, numGraphEdges))
+            message.maxBy(x => modularityGain(attr, x))
           },
 
         e => {
-          if (isModularityGained(ufWorkGraph, e.dstAttr, e.srcAttr, numGraphEdges)) {
+          if (isModularityGained(e.dstAttr, e.srcAttr)) {
             Iterator(
               (e.dstId, List(e.srcAttr.union(e.dstAttr))),
               (e.srcId, List(e.srcAttr.union(e.dstAttr)))
