7e436306b27fbd9e61479706265d58d3ba8fdfe6
Xusen Yin <yinxusen@gmail.com>
2014-12-22 21:24:17 +0800

    add nullable columns in tpcds

--- mllib/src/main/scala/org/apache/spark/mllib/tree/TpcDSDemo.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/tree/TpcDSDemo.scala
@@ -1,20 +1,47 @@
 package org.apache.spark.mllib.tree
 
 import org.apache.spark.sql.SQLContext
-import org.apache.spark.sql.catalyst.expressions.Row
-import org.apache.spark.sql.catalyst.types._
 import org.apache.spark.{SparkContext, SparkConf}
+import scala.collection._
+import StringUtils._
+
+object StringUtils {
+  implicit class StringImprovements(val s: String) {
+    def getColumns(s: String, sep: Char): Array[String] = {
+      val sb = new StringBuilder()
+      val res = mutable.MutableList[String]()
+      var i = 0
+      while (i < s.size) {
+        if (s(i) == sep) {
+          res += sb.toString()
+          sb.clear()
+        } else {
+          sb.append(s(i))
+        }
+        i += 1
+      }
+      res.toArray
+    }
+    import scala.util.control.Exception._
+    def toIntOpt = catching(classOf[NumberFormatException]) opt s.toInt
+    def toFloatOpt = catching(classOf[NumberFormatException]) opt s.toFloat
+    def toStringOpt = if (s == "") None else Some(s)
+    def toIntOr(default: Int) = s.toIntOpt.getOrElse(default)
+    def toFloatOr(default: Float) = s.toFloatOpt.getOrElse(default)
+    def toColumns = getColumns(s, '|')
+  }
+}
 
 case class CustomerDemographics(
     cdDemoSk: Int,
-    cdGender: String,
-    cdMaritalStatus: String,
-    cdEducationStatus: String,
-    cdPurchaseEstimate: Int,
-    cdCreditRating: String,
-    cdDepCount: Int,
-    cdDepEmployedCount: Int,
-    cdDepCollegeCount: Int
+    cdGender: Option[String],
+    cdMaritalStatus: Option[String],
+    cdEducationStatus: Option[String],
+    cdPurchaseEstimate: Option[Int],
+    cdCreditRating: Option[String],
+    cdDepCount: Option[Int],
+    cdDepEmployedCount: Option[Int],
+    cdDepCollegeCount: Option[Int]
 )
 
 object CustomerDemographics {
@@ -22,92 +49,70 @@ object CustomerDemographics {
     assert(splits.size == 9)
     CustomerDemographics(
       splits(0).toInt,
-      splits(1),
-      splits(2),
-      splits(3),
-      splits(4).toInt,
-      splits(5),
-      splits(6).toInt,
-      splits(7).toInt,
-      splits(8).toInt
-    )
+      splits(1).toStringOpt,
+      splits(2).toStringOpt,
+      splits(3).toStringOpt,
+      splits(4).toIntOpt,
+      splits(5).toStringOpt,
+      splits(6).toIntOpt,
+      splits(7).toIntOpt,
+      splits(8).toIntOpt)
   }
 }
 
-class StoreSales(
-    val ssSoldDateSk: Int,
-    val ssSoldTimeSk: Int,
-    val ssItemSk: Int,
-    val ssCustomerSk: Int,
-    val ssCDemoSk: Int,
-    val ssHDemoSk: Int,
-    val ssAddrSk: Int,
-    val ssStoreSk: Int,
-    val ssPromoSk: Int,
-    val ssTicketNumber: Int,
-    val ssQuantity: Int,
-    val ssWholeSaleCost: Float,
-    val ssListPrice: Float,
-    val ssSalesPrice: Float,
-    val ssExtDiscountAmt: Float,
-    val ssExtSalesPrice: Float,
-    val ssExtWholeSaleCost: Float,
-    val ssExtListPrice: Float,
-    val ssExtTax: Float,
-    val ssCouponAmt: Float,
-    val ssNetPaid: Float,
-    val ssNetPaidIncTax: Float,
-    val ssNetProfit: Float
+case class StoreSales(
+    ssSoldDateSk: Option[Int],
+    ssSoldTimeSk: Option[Int],
+    ssItemSk: Int,
+    ssCustomerSk: Option[Int],
+    ssCDemoSk: Option[Int],
+    ssHDemoSk: Option[Int],
+    ssAddrSk: Option[Int],
+    ssStoreSk: Option[Int],
+    ssPromoSk: Option[Int],
+    ssTicketNumber: Int,
+    ssQuantity: Option[Int],
+    ssWholeSaleCost: Option[Float],
+    ssListPrice: Option[Float],
+    ssSalesPrice: Option[Float],
+    ssExtDiscountAmt: Option[Float],
+    ssExtSalesPrice: Option[Float],
+    ssExtWholeSaleCost: Option[Float],
+    ssExtListPrice: Option[Float],
+    ssExtTax: Option[Float],
+    ssCouponAmt: Option[Float],
+    ssNetPaid: Option[Float],
+    ssNetPaidIncTax: Option[Float],
+    ssNetProfit: Option[Float]
 )
 
 object StoreSales {
   def apply(splits: Array[String]): StoreSales = {
     assert(splits.size == 23)
     new StoreSales(
-      splits(0).toInt,
-      splits(1).toInt,
+      splits(0).toIntOpt,
+      splits(1).toIntOpt,
       splits(2).toInt,
-      splits(3).toInt,
-      splits(4).toInt,
-      splits(5).toInt,
-      splits(6).toInt,
-      splits(7).toInt,
-      splits(8).toInt,
+      splits(3).toIntOpt,
+      splits(4).toIntOpt,
+      splits(5).toIntOpt,
+      splits(6).toIntOpt,
+      splits(7).toIntOpt,
+      splits(8).toIntOpt,
       splits(9).toInt,
-      splits(10).toInt,
-      splits(11).toInt,
-      splits(12).toFloat,
-      splits(13).toFloat,
-      splits(14).toFloat,
-      splits(15).toFloat,
-      splits(16).toFloat,
-      splits(17).toFloat,
-      splits(18).toFloat,
-      splits(19).toFloat,
-      splits(20).toFloat,
-      splits(21).toFloat,
-      splits(22).toFloat
-    )
-  }
-
-  def getName = {
-    getClass.getCanonicalName.dropWhile(_ == '$')
-  }
-
-  def getSchema = {
-    StructType(
-      Class.forName(getName).getDeclaredFields.map { field =>
-        val name = field.getName
-        val tpe = field.getType match {
-          case Int =>
-            IntegerType.asInstanceOf[DataType]
-          case Float =>
-            FloatType.asInstanceOf[DataType]
-          case _ =>
-            DataType.asInstanceOf[DataType]
-        }
-        StructField(name, tpe)
-      }
+      splits(10).toIntOpt,
+      splits(11).toFloatOpt,
+      splits(12).toFloatOpt,
+      splits(13).toFloatOpt,
+      splits(14).toFloatOpt,
+      splits(15).toFloatOpt,
+      splits(16).toFloatOpt,
+      splits(17).toFloatOpt,
+      splits(18).toFloatOpt,
+      splits(19).toFloatOpt,
+      splits(20).toFloatOpt,
+      splits(21).toFloatOpt,
+      splits(22).toFloatOpt
     )
   }
 }
@@ -117,6 +122,7 @@ class TpcDSDemo {
 }
 
 object TpcDSDemo {
+
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("TPC-DS DEMO").setMaster("local[4]")
     val sc = new SparkContext(conf)
@@ -126,7 +132,7 @@ object TpcDSDemo {
 
     val customerDemographics = sc
       .textFile("/home/sen/data/tpcds-data/customer_demographics.dat", minPartitions)
-      .map(line => CustomerDemographics(line.split('|'))).toSchemaRDD
+      .map(l => CustomerDemographics(l.toColumns)).toSchemaRDD
 
     registerRDDAsTable(customerDemographics, "customer_demographics")
 
@@ -135,13 +141,11 @@ object TpcDSDemo {
 
     val storeSales = sc
       .textFile("/home/sen/data/tpcds-data/store_sales.dat", minPartitions)
-      .map(line => Row(line.split('|'): _*))
-    val storeSalesSchema = StoreSales.getSchema
-    val storeSalesSchemaRDD = applySchema(storeSales, storeSalesSchema)
+      .map(l => StoreSales(l.toColumns)).toSchemaRDD
 
-    registerRDDAsTable(storeSalesSchemaRDD, "store_sales")
+    registerRDDAsTable(storeSales, "store_sales")
 
-    sql("select ssItemSk, ssCustomerSk from store_sales limit 10")
-      .foreach(r => println(s"${r.getInt(0)}, ${r.getInt(1)}"))
+    sql("select ssItemSk, ssCustomerSk from store_sales")
+      .foreach(r => println(s"${if (r.isNullAt(0)) "null" else r.getInt(0)}, ${if (r.isNullAt(1)) "null" else r.getInt(1)}"))
   }
 }
