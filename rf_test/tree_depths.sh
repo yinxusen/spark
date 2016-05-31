89c8e6ece677dbfeb64695ff8540539ac74d536b
yinxusen <yinxusen@gmail.com>
2016-05-30 18:54:42 -0700

    add for new test

--- rf_test/tree_depths.sh
+++ rf_test/tree_depths.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-#../bin/run-example ml.AirlineRFC ~/data/airline/train-10m.csv ~/data/airline/test.csv --numTrees 1 --maxDepth 5 
-#../bin/run-example ml.AirlineRFC ~/data/airline/train-10m.csv ~/data/airline/test.csv --numTrees 1 --maxDepth 10
-../bin/run-example ml.AirlineRFC ~/data/airline/train-10m.csv ~/data/airline/test.csv --numTrees 1 --maxDepth 20 --maxMemoryInMB 500 --numPartitions 1
+../bin/run-example ml.AirlineRFC ~/data/airline/train-0.01m.csv ~/data/airline/test.csv --numTrees 10 --maxDepth 20 
+../bin/run-example ml.AirlineRFC ~/data/airline/train-0.1m.csv ~/data/airline/test.csv --numTrees 10 --maxDepth 20
+../bin/run-example ml.AirlineRFC ~/data/airline/train-1m.csv ~/data/airline/test.csv --numTrees 10 --maxDepth 20
+../bin/run-example ml.AirlineRFC ~/data/airline/train-10m.csv ~/data/airline/test.csv --numTrees 10 --maxDepth 20 --maxMemoryInMB 500 --numPartitions 2
