b90a0bbe17e5d2650c7583898f219e7835791e39
Xusen Yin <yinxusen@gmail.com>
2016-12-08 14:41:14 -0800

    add perf test

--- sql/core/src/test/scala/org/apache/spark/sql/DatasetToArrowPerfSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/DatasetToArrowPerfSuite.scala
@@ -36,66 +36,52 @@ import org.apache.spark.unsafe.types.UTF8String
 import org.apache.spark.util.Utils
 
 
-case class ArrowTestClass(col1: Int, col2: Double, col3: String)
-
-class DatasetToArrowSuite extends QueryTest with SharedSQLContext {
+class DatasetToArrowPerfSuite extends QueryTest with SharedSQLContext {
 
   import testImplicits._
+  import DatasetToArrowPerfSuite.time
 
-  final val numElements = 4
-  @transient var data: Seq[ArrowTestClass] = _
+  final val numElements = 4 * 100000
+  @transient var data: Dataset[ArrowTestClass] = _
 
   override def beforeAll(): Unit = {
     super.beforeAll()
     data = Seq.fill(numElements)(ArrowTestClass(
-      Random.nextInt, Random.nextDouble, Random.nextString(Random.nextInt(100))))
+      Random.nextInt, Random.nextDouble, Random.nextString(Random.nextInt(100)))).toDS().cache()
+    data.collect()
   }
 
-  test("Collect as arrow to python") {
-    val dataset = data.toDS()
-
-    val port = dataset.collectAsArrowToPython()
-
-    val receiver: RecordBatchReceiver = new RecordBatchReceiver
-    val (buffer, numBytesRead) = receiver.connectAndRead(port)
-    val channel = receiver.makeFile(buffer)
-    val reader = new ArrowReader(channel, receiver.allocator)
-
-    val footer = reader.readFooter()
-    val schema = footer.getSchema
-
-    val numCols = schema.getFields.size()
-    assert(numCols === dataset.schema.fields.length)
-    for (i <- 0 until schema.getFields.size()) {
-      val arrowField = schema.getFields.get(i)
-      val sparkField = dataset.schema.fields(i)
-      assert(arrowField.getName === sparkField.name)
-      assert(arrowField.isNullable === sparkField.nullable)
-      assert(DatasetToArrowSuite.compareSchemaTypes(arrowField, sparkField))
-    }
-
-    val blockMetadata = footer.getRecordBatches
-    assert(blockMetadata.size() === 1)
-
-    val recordBatch = reader.readRecordBatch(blockMetadata.get(0))
-    val nodes = recordBatch.getNodes
-    assert(nodes.size() === numCols + 1)  // +1 for Type String, which has two nodes.
-
-    val firstNode = nodes.get(0)
-    assert(firstNode.getLength === numElements)
-    assert(firstNode.getNullCount === 0)
-
-    val buffers = recordBatch.getBuffers
-    assert(buffers.size() === (numCols + 1) * 2)  // +1 for Type String
-
-    assert(receiver.getIntArray(buffers.get(1)) === data.map(_.col1))
-    assert(receiver.getDoubleArray(buffers.get(3)) === data.map(_.col2))
-    assert(receiver.getStringArray(buffers.get(5), buffers.get(7)) ===
-      data.map(d => UTF8String.fromString(d.col3)).toArray)
+  test("Performance test of Arrow") {
+    var numBytesRead = 0
+    time {
+      val port = data.collectAsArrowToPython()
+      val receiver: RecordBatchReceiver = new RecordBatchReceiver
+      val (buffer, cnt) = receiver.connectAndRead(port)
+      numBytesRead = cnt
+      val channel = receiver.makeFile(buffer)
+    }("Arrow")
+    // scalastyle:off println
+    println(s"Size of Arrow is $numBytesRead")
+    // scalastyle:on println
   }
+
+  test("Performance test of AutoBatchPickler") {
+    var numBytesRead = 0
+    time {
+      val port = data.collectToPython()
+      val receiver: RecordBatchReceiver = new RecordBatchReceiver
+      val (buffer, cnt) = receiver.connectAndReadPickler(port)
+      numBytesRead = cnt
+      val channel = receiver.makeFile(buffer)
+    }("AutoBatchPickler")
+    // scalastyle:off println
+    println(s"Size of AutoBatchPickler is $numBytesRead")
+    // scalastyle:on println
+  }
+
 }
 
-object DatasetToArrowSuite {
+object DatasetToArrowPerfSuite {
   def compareSchemaTypes(arrowField: Field, sparkField: StructField): Boolean = {
     val arrowType = arrowField.getType
     val sparkType = sparkField.dataType
@@ -112,8 +98,19 @@ object DatasetToArrowSuite {
       case _ => false
     }
   }
+
+  def time[R](block: => R)(blockName: String = "Unknown Block"): R = {
+    val t0 = System.nanoTime()
+    val result: R = block
+    val t1 = System.nanoTime()
+    // scalastyle:off println
+    println(s"Elapsed time of $blockName is ${(t1 - t0) * 1.0 / 1000 / 1000} ms.")
+    // scalastyle:on println
+    result
+  }
 }
 
+/*
 class RecordBatchReceiver {
 
   val allocator = new RootAllocator(Long.MaxValue)
@@ -159,20 +156,6 @@ class RecordBatchReceiver {
     (buffer, messageLength)
   }
 
-  def connectAndReadPickler(port: Int): (Array[Byte], Int) = {
-    val batchSize = 1024
-    val clientSocket = new Socket(InetAddress.getByName("localhost"), port)
-    val clientDataIns = new DataInputStream(clientSocket.getInputStream)
-    val buffer = Array.ofDim[Byte](batchSize)
-    var total = 0
-    var n = clientDataIns.read(buffer, 0, batchSize)
-    while (n > 0) {
-      total += n
-      n = clientDataIns.read(buffer, 0, batchSize)
-    }
-    (buffer, total + 1)
-  }
-
   def makeFile(buffer: Array[Byte]): FileChannel = {
     val tempDir = Utils.createTempDir(namePrefix = this.getClass.getName).getPath
     val arrowFile = new File(tempDir, "arrow-bytes")
@@ -184,3 +167,4 @@ class RecordBatchReceiver {
     arrowIns.getChannel
   }
 }
+*/
